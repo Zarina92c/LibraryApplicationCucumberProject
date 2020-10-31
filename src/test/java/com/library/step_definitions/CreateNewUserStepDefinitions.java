61fcf85b9b2f8e64199a55008ae555fbbc7ecd62
HakkiSabanci <hakkisabanci@yahoo.com>
2020-10-31 15:31:01 -0400

    created new feature step definitions for CreateNewUserStepDefinitions

--- src/test/java/com/library/step_definitions/CreateNewUserStepDefinitions.java
+++ src/test/java/com/library/step_definitions/CreateNewUserStepDefinitions.java
@@ -1,6 +1,6 @@
 package com.library.step_definitions;
 
-public class Hakki {
+public class CreateNewUserStepDefinitions {
     public static void main(String[] args) {
         System.out.println("hello");
     }
