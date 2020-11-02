536ed93e1fa463debb636308e35fc0e91b9c66ba
HakkiSabanci <hakkisabanci@yahoo.com>
2020-11-01 21:50:28 -0500

    all updated new librarian account methods and Elements

--- src/test/java/com/library/pages/UserManagementPage.java
+++ src/test/java/com/library/pages/UserManagementPage.java
@@ -1,19 +1,113 @@
 package com.library.pages;
 
+import com.github.javafaker.Faker;
+import com.library.utils.BrowserWait;
+import com.library.utils.Driver;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
 
 public class UserManagementPage extends LoginPage{
 
     @FindBy(xpath = "//i[@class='fa fa-plus']")
-    WebElement addUserButton;
+    protected WebElement addUserButton;
 
     public  void click_addUserButton(){
         addUserButton.click();
     }
 
-    @FindBy(xpath = "//tr[1]/td[2]")
-    WebElement userIDBox;
+    @FindBy(xpath = "//div[.='The user has been created.']")
+    WebElement newUserCreatedMessage;
+
+    //Add User boxes WebElements
+    @FindBy(name = "full_name")
+    protected WebElement fullNameInbox;
+
+    @FindBy(name="password")
+    protected WebElement passwordInbox;
+
+    @FindBy(name="email")
+    protected WebElement emailInbox;
+
+    @FindBy(name = "start_date")
+    protected WebElement startDatePicker;
+
+    @FindBy(name ="end_date")
+    protected WebElement endDatePicker;
+
+    @FindBy(id = "address")
+    protected WebElement addressInbox;
+
+    @FindBy(id="user_group_id")
+    protected WebElement userGroupDropDown;
+
+    @FindBy(id = "status")
+    protected WebElement statusDropDown;
+
+    @FindBy(xpath = "//button[.='Save changes']")
+    protected WebElement saveChangesButton;
+
+    @FindBy(xpath = "//button[.='Close']")
+    protected WebElement closeButton;
+
+    /**
+     *
+     * @param str is for userGroup can be only student or librarian
+     */
+    public void addUser(String str){
+        Faker faker =new Faker();
+        String fullName=faker.name().fullName();
+        String password=faker.number().digits(5);
+        String usernameEmail = faker.name().username();
+        String companyUrl= faker.company().url().substring(4);
+        String email = usernameEmail+"@"+companyUrl;
+        String address= faker.address().fullAddress();
+        if (str.equalsIgnoreCase("student")){
+            fullNameInbox.sendKeys(fullName);
+            passwordInbox.sendKeys(password);
+            emailInbox.sendKeys(email);
+            addressInbox.sendKeys(address);
+            Select userGroup = new Select(userGroupDropDown);
+            userGroup.selectByValue("3");
+            endDatePicker.clear();
+            endDatePicker.click();
+            endDatePicker.sendKeys(Keys.ENTER);
+           /* endDatePicker.click();
+            BrowserWait.wait(1);
+            WebElement yearPicker = Driver.getDriver().findElement(By.xpath("/html/body/div[2]/div[1]/table/thead/tr[1]/th[2]"));
+            yearPicker.click();*/
+        }else if (str.equalsIgnoreCase("librarian")){
+            fullNameInbox.sendKeys(fullName);
+            passwordInbox.sendKeys(password);
+            emailInbox.sendKeys(email);
+            addressInbox.sendKeys(address);
+            Select userGroup = new Select(userGroupDropDown);
+            userGroup.selectByValue("2");
+            endDatePicker.clear();
+            endDatePicker.click();
+            endDatePicker.sendKeys(Keys.ENTER);
+           /* endDatePicker.click();
+            BrowserWait.wait(1);
+            WebElement yearPicker = Driver.getDriver().findElement(By.xpath("/html/body/div[2]/div[1]/table/thead/tr[1]/th[2]"));
+            yearPicker.click();*/
+        }else{
+            throw new RuntimeException("There is not such a named '"+str+"'  user");
+        }
+    }
+
+    public void click_saveChangeButton(){
+        saveChangesButton.click();
+    }
+    public void click_closeButton(){
+        closeButton.click();
+    }
+    public void assertingNewUserCreated(){
+        Assert.assertTrue(newUserCreatedMessage.isDisplayed());
+    }
+
 
 
 
--- src/test/java/com/library/step_definitions/CreateNewUserStepDefinitions.java
+++ src/test/java/com/library/step_definitions/CreateNewUserStepDefinitions.java
@@ -23,14 +23,14 @@ public class CreateNewUserStepDefinitions {
     }
     @When("user adds new user information as student type")
     public void user_adds_new_user_information_as_student_type() {
-
+        userManagementPage.addUser("librarian");
     }
     @When("user clicks on save changes button")
     public void user_clicks_on_save_changes_button() {
-
+        userManagementPage.click_saveChangeButton();
     }
     @Then("user should see new user top of the list")
     public void user_should_see_new_user_top_of_the_list() {
-
+        userManagementPage.assertingNewUserCreated();
     }
 }
