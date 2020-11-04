41a7b53b8b5e88ef5b83234db2bd9db7c48f7e9a
illofca <68875931+illofca@users.noreply.github.com>
2020-11-04 00:06:18 -0500

    LibrarianSortUsersPage explicit wait method updated

--- src/test/java/com/library/pages/LibrarianSortUsersPage.java
+++ src/test/java/com/library/pages/LibrarianSortUsersPage.java
@@ -1,9 +1,14 @@
 package com.library.pages;
 
 
+import com.library.utils.BrowserWait;
+import com.library.utils.Driver;
 import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LibrarianSortUsersPage extends LoginPage{
 
@@ -14,18 +19,26 @@ public class LibrarianSortUsersPage extends LoginPage{
     @FindBy(xpath = "//*[@id=\"tbl_users\"]/thead/tr/th[6]")
     protected WebElement statusBtn;
 
-
     public void clickOnGroup() {
+
         groupBtn.click();
+       // BrowserWait.wait(5);
 
     }
-    public void clickOnStatus() {statusBtn.click();
+    public void clickOnStatus() {
+        statusBtn.click();
+
     }
+
 
     public void assertGroupSorting(){
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 10);
+        wait.until(ExpectedConditions.visibilityOf(groupBtn));
         String sortingValue = groupBtn.getAttribute("aria-label");
         Assert.assertTrue(sortingValue.contains("descending"));
+
     }
+
 
 
     }
--- src/test/java/com/library/runners/CucumberRunner.java
+++ src/test/java/com/library/runners/CucumberRunner.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
 
         features ="src/test/resources/features",
         glue = "com/library/step_definitions",
-        dryRun = true,
+        dryRun = false,
         tags = "@sort_users",
         publish = false
 
