ed1a1a83b71c1a9e288599f74e43769d169b5a23
illofca <68875931+illofca@users.noreply.github.com>
2020-11-05 22:18:44 -0500

    Trying to push all

--- src/test/java/com/library/runners/CucumberRunner.java
+++ src/test/java/com/library/runners/CucumberRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         glue = "com/library/step_definitions",
         dryRun = false,
         tags = "@Smoke",
-        publish = false
+        publish = true
 
 
 )
