da89509a0dc7803d0263a8f8e0a27006f5d44c15
Zarina92c <zkurtbedinova@gmail.com>
2020-11-08 16:09:17 -0500

    hooks

--- src/test/java/com/library/runners/FailedRunner.java
+++ src/test/java/com/library/runners/FailedRunner.java
@@ -1,4 +1,14 @@
 package com.library.runners;
 
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        glue = "com/library/step_definitions",
+        features = "@target/rerun.txt"
+)
 public class FailedRunner {
 }
