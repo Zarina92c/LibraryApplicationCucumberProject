6cc5fb1a8a25d31f3f6d09b140a7d212b1339dd2
Zarina92c <zkurtbedinova@gmail.com>
2020-11-08 16:20:21 -0500

    runner

--- src/test/java/com/library/runners/CucumberRunner.java
+++ src/test/java/com/library/runners/CucumberRunner.java
@@ -7,8 +7,12 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions (
+        plugin = {
+                "rerun:target/rerun.txt",
+                "json:target/cucumber.json"
+        },
 
-        plugin = "html:target/cucumber-report.html",
+      //  plugin = "html:target/cucumber-report.html",
         features ="src/test/resources/features",
         glue = "com/library/step_definitions",
         dryRun = false,
