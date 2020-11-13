3403d20376e36d42b3ab7144175737d6e93879fb
Zarina92c <zkurtbedinova@gmail.com>
2020-11-13 18:26:38 -0500

    driver class

--- src/test/java/com/library/utils/Driver.java
+++ src/test/java/com/library/utils/Driver.java
@@ -4,6 +4,10 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URL;
 
 public class Driver {
     private static WebDriver driver;
@@ -26,12 +30,41 @@ public class Driver {
                     WebDriverManager.firefoxdriver().setup();
                     driver = new FirefoxDriver();
                     break;
-                default:
-                    throw new RuntimeException("No such a browser yet!");
-            }
+
+            case "remote-chrome":
+                try{
+                    DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+                    desiredCapabilities.setBrowserName("chrome");
+                    URL gridUrl = new URL ("http://54.82.123.241:4444/wd/hub");
+                    driver = new RemoteWebDriver(gridUrl, desiredCapabilities);
+
+                }catch (Exception e) {
+                    e.printStackTrace();
+                }
+                break;
+
+                case "remote-firefox":
+                    try{
+                        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+                        desiredCapabilities.setBrowserName("firefox");
+                        URL gridUrl = new URL ("http://54.82.123.241:4444/wd/hub");
+                        driver = new RemoteWebDriver(gridUrl, desiredCapabilities);
+
+                    }catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    break;
+
+            default:
+                throw new RuntimeException("No such a browser yet!");
+        }
+
+
         }
         return driver;
     }
+
+
 
     public static void closeDriver() {
         if (driver != null) {
