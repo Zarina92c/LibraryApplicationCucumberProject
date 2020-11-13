8f4cb4fde60ee47862144e68c0f02395ede9bc98
Zarina92c <zkurtbedinova@gmail.com>
2020-11-13 16:28:26 -0500

    delete hakki file

--- src/test/java/com/library/runners/CucumberRunner.java
+++ src/test/java/com/library/runners/CucumberRunner.java
@@ -17,7 +17,7 @@ import org.junit.runner.RunWith;
         features ="src/test/resources/features",
         glue = "com/library/step_definitions",
         dryRun = false,
-        tags = "@edit_user_information",
+        tags = "@librarian",
         publish = true
 
 )
